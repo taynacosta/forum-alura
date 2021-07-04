9d97541bcddf5889b677b79f26c07d6d6d50a855
taynacosta <taynamoreira1811@gmail.com>
2021-07-04 16:30:43 -0300

    autenticacao de usuario

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$t/ZLx/1toPUG/m96UtA0C.QufCgA2EMMz88s6ZfIk3k/fHILJ9AeK');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
