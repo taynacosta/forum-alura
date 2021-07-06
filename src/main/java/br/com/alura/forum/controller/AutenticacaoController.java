177215e6c3fc8f5c8f091cf337f876ae56f395b0
taynacosta <taynamoreira1811@gmail.com>
2021-07-06 07:26:43 -0300

    Recuperando Token Authorization

--- src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
+++ src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.config.security.TokenService;
 import br.com.alura.forum.controller.Form.FormLogin;
+import br.com.alura.forum.controller.dto.TokenDto;
 
 @RestController
 @RequestMapping("/auth")
@@ -28,13 +29,12 @@ public class AutenticacaoController {
 	private TokenService tokenService;
 	
 	@PostMapping
-	public ResponseEntity<?> autenticar(@RequestBody @Valid FormLogin form){
+	public ResponseEntity<TokenDto> autenticar(@RequestBody @Valid FormLogin form){
 		UsernamePasswordAuthenticationToken dadosLogin = form.converter();
 		try{
 			Authentication authentication = authManager.authenticate(dadosLogin);
 			String token = tokenService.gerarToken(authentication);
-			System.out.println(token);
-			return ResponseEntity.ok().build();
+			return ResponseEntity.ok(new TokenDto(token, "Bearer"));
 			
 		}
 		catch(AuthenticationException e){
